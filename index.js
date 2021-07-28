a81859af74cb830b32dfeeaf3731bbdd6f7befd7
ciamac-da <ciamac.davoudi@gmail.com>
2021-07-28 22:27:02 +0200

    Verified by Marti4n v1.0.5 display balance

--- index.js
+++ index.js
@@ -32,9 +32,32 @@ ${transaction.text}
 list.appendChild(item)
 }
 
+// Update the balance, income and expense
+const updateValues = () => {
+  const amounts = transactions.map(transaction => {
+  return  transaction.amount
+  })
+  const total = amounts.reduce( (acc,item) => (acc += item),0).toFixed(2)
+
+  const income = amounts
+  .filter(item => item > 0)
+  .reduce((acc, item) => (acc += item), 0)
+  .toFixed(2)
+
+  const expense = ( amounts
+  .filter(item => item < 0)
+  .reduce((acc, item) => (acc += item), 0) * -1)
+  .toFixed(2)
+
+  balance.innerText = `$${total}`
+  money_plus.innerText = `$${income}`
+  money_minus.innerText = `$${expense}`
+}
+
 // Init app
 const init = () => {
   list.innerHTML = "";
   transactions.forEach(addTransactionToDOM)
+  updateValues()
 }
 init()
